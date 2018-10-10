8843bd6a840f5f646b677d028f4442342a1f0921
wanessamarcondes <wanessamarcondes@gmail.com>
2018-10-10 11:28:00 -0300

    reset button fixed

--- main.js
+++ main.js
@@ -35,138 +35,154 @@ function getAdjacentCellIndexes(x, y) {
     });
 }
 
-var field_matrix = [];
-var field = $("#field table");
-for (var i = 0; i < HEIGHT; i++) {
-    var row_vector = [];
-    var row = $("<tr>");
-    for (var j = 0; j < WIDTH; j++) {
-        var cell = $("<td>");
-        cell.data("mines", 0);
+$("#reset").click(function(){
+    clearInterval(TIMER);
+    TIMER = false;
+    $(this).removeClass("game-over winner wow");
+    $("#mines").text("");
+    $("#timer").text("");
+    $("#field table").empty();
+    minesweeper();
+})
 
-        var button = $("<div>");
-        button.addClass("button");
-        button.data("coordinates", [j, i]);
+function minesweeper(){
 
-        button.contextmenu(function () {
-            return false;
-        });
+    var field_matrix = [];
+    var field = $("#field table");
+    for (var i = 0; i < HEIGHT; i++) {
+        var row_vector = [];
+        var row = $("<tr>");
+        for (var j = 0; j < WIDTH; j++) {
+            var cell = $("<td>");
+            cell.data("mines", 0);
 
-        button.mousedown(function(event) {
-            if (!TIMER) {
-                TIMER = setInterval(function () {
-                    counter++;
-                    $("#timer").text(counter);
-                }, 1000);
-            }
-            if (event.which === 3) {
-                $(this).toggleClass("red-flag");
-                $("#mines").text($(".red-flag").length);
-            } else {
-                $("#reset").addClass("wow");
-            }
-        });
+            var button = $("<div>");
+            button.addClass("button");
+            button.data("coordinates", [j, i]);
 
-        button.mouseup(function () {
-            $("#reset").removeClass("wow");
-            if (!$(this).hasClass("red-flag")) {
-                if ($(this).parent().hasClass("mine")) {
-                    $("td .button").each(function (index, button) {
-                        button.remove();
-                    })
-                    $("#reset").addClass("game-over");
-                    clearInterval(TIMER);
-                } else if ($(this).parent().data("mines") > 0) {
-                    $(this).remove();
-                } else if ($(this).parent().data("mines") === 0) {
-                    var coordinates = $(this).data("coordinates");
-                    $(this).remove();
-                    (function (x, y) {
-                        var adjacent_cells = getAdjacentCellIndexes(x, y);
-                        for (var k = 0; k < adjacent_cells.length; k++) {
-                            var x = adjacent_cells[k][0];
-                            var y = adjacent_cells[k][1];
-                            var cell = $(field_matrix[y][x]);
-                            var button = cell.children($(".button"));
-                            if (button.length > 0) {
-                                button.remove();
-                                if (cell.data("mines") === 0) {
-                                    arguments.callee(x, y);
+            button.contextmenu(function () {
+                return false;
+            });
+
+            counter = 0;
+            button.mousedown(function(event) {
+                if (!TIMER) {
+                    TIMER = setInterval(function () {
+                        counter++;
+                        $("#timer").text(counter);
+                    }, 1000);
+                }
+                if (event.which === 3) {
+                    $(this).toggleClass("red-flag");
+                    $("#mines").text($(".red-flag").length);
+                } else {
+                    $("#reset").addClass("wow");
+                }
+            });
+
+            button.mouseup(function () {
+                $("#reset").removeClass("wow");
+                if (!$(this).hasClass("red-flag")) {
+                    if ($(this).parent().hasClass("mine")) {
+                        $("td .button").each(function (index, button) {
+                            button.remove();
+                        })
+                        $("#reset").addClass("game-over");
+                        clearInterval(TIMER);
+                    } else if ($(this).parent().data("mines") > 0) {
+                        $(this).remove();
+                    } else if ($(this).parent().data("mines") === 0) {
+                        var coordinates = $(this).data("coordinates");
+                        $(this).remove();
+                        (function (x, y) {
+                            var adjacent_cells = getAdjacentCellIndexes(x, y);
+                            for (var k = 0; k < adjacent_cells.length; k++) {
+                                var x = adjacent_cells[k][0];
+                                var y = adjacent_cells[k][1];
+                                var cell = $(field_matrix[y][x]);
+                                var button = cell.children($(".button"));
+                                if (button.length > 0) {
+                                    button.remove();
+                                    if (cell.data("mines") === 0) {
+                                        arguments.callee(x, y);
+                                    }
                                 }
                             }
-                        }
-                    })(coordinates[0], coordinates[1]);
+                        })(coordinates[0], coordinates[1]);
+                    }
+
+                    if ($("td .button").length === MINES) {
+                        $("#reset").addClass("winner");
+                        clearInterval(TIMER);
+                    }
+
                 }
+            })
 
-                if ($("td .button").length === MINES) {
-                    $("#reset").addClass("winner");
-                    clearInterval(TIMER);
-                }
+            cell.append(button);
 
-            }
-        })
-
-        cell.append(button);
-
-        row.append(cell);
-        row_vector.push(cell)
+            row.append(cell);
+            row_vector.push(cell)
+        }
+        field.append(row);
+        field_matrix.push(row_vector);
     }
-    field.append(row);
-    field_matrix.push(row_vector);
-}
 
-var mine_indexes = getUniqueRandomIndexesInField(field_matrix);
-$.each(mine_indexes, function(index, coordinates) {
-    var x = coordinates[0];
-    var y = coordinates[1];
-    var mine = $(field_matrix[y][x]);
-    mine.addClass("mine");
-});
-
-$.each(mine_indexes, function (index, coordinates) {
-    var adjacent_cells = getAdjacentCellIndexes(coordinates[0], coordinates[1]);
-    $.each(adjacent_cells, function(index, coordinates) {
+    var mine_indexes = getUniqueRandomIndexesInField(field_matrix);
+    $.each(mine_indexes, function(index, coordinates) {
         var x = coordinates[0];
         var y = coordinates[1];
-        var cell = $(field_matrix[y][x]);
-        if (!cell.hasClass("mine")) {
-            var num_mines = cell.data("mines") + 1;
-            cell.data("mines", num_mines);
-            switch (num_mines) {
-                case 1:
-                    cell.css("color", "blue");
-                    break;
-                case 2:
-                    cell.css("color", "green");
-                    break;
-                case 3:
-                    cell.css("color", "red");
-                    break;
-                case 4:
-                    cell.css("color", "navy");
-                    break;
-                case 5:
-                    cell.css("color", "maroon");
-                    break;
-                case 6:
-                    cell.css("color", "teal");
-                    break;
-                case 7:
-                    cell.css("color", "DarkMagenta");
-                    break;
-                case 8:
-                    cell.css("color", "black");
-                    break;
-            }
-        }
-    })
-});
-
-$.each(field_matrix, function(index, row) {
-    $.each(row, function(index, cell) {
-        var number = $(cell).data("mines");
-        if (number > 0) {
-            $(cell).append(number);
-        }
+        var mine = $(field_matrix[y][x]);
+        mine.addClass("mine");
     });
-});
+
+    $.each(mine_indexes, function (index, coordinates) {
+        var adjacent_cells = getAdjacentCellIndexes(coordinates[0], coordinates[1]);
+        $.each(adjacent_cells, function(index, coordinates) {
+            var x = coordinates[0];
+            var y = coordinates[1];
+            var cell = $(field_matrix[y][x]);
+            if (!cell.hasClass("mine")) {
+                var num_mines = cell.data("mines") + 1;
+                cell.data("mines", num_mines);
+                switch (num_mines) {
+                    case 1:
+                        cell.css("color", "blue");
+                        break;
+                    case 2:
+                        cell.css("color", "green");
+                        break;
+                    case 3:
+                        cell.css("color", "red");
+                        break;
+                    case 4:
+                        cell.css("color", "navy");
+                        break;
+                    case 5:
+                        cell.css("color", "maroon");
+                        break;
+                    case 6:
+                        cell.css("color", "teal");
+                        break;
+                    case 7:
+                        cell.css("color", "DarkMagenta");
+                        break;
+                    case 8:
+                        cell.css("color", "black");
+                        break;
+                }
+            }
+        })
+    });
+
+    $.each(field_matrix, function(index, row) {
+        $.each(row, function(index, cell) {
+            var number = $(cell).data("mines");
+            if (number > 0) {
+                $(cell).append(number);
+            }
+        });
+    });
+}
+
+minesweeper();
